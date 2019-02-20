c2c672ba57a896adb662aa4454cbeebc4fa5e3a7
Kyle <Kyl.johnson@gmail.com>
2019-02-20 09:47:44 -0800

    Initial commit

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,21 @@
+# app/controllers/logins_controller.rb
+class LoginsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = authenticate_with_google
+      cookies.signed[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to login_url, alert: 'authentication_failed'
+    end
+  end
+
+  private
+    def authenticate_with_google
+      if flash[:google_sign_in_token].present?
+        User.find_by google_id: GoogleSignIn::Identity.new(flash[:google_sign_in_token]).user_id
+      end
+    end
+end
